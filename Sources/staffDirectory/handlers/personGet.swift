58f955acfa7586fac08a3e2867c830ae818bd761
Jonathan Guthrie <jono@perfect.org>
2017-04-14 12:02:42 -0400

    comment code, plus a few small refinements

--- Sources/staffDirectory/handlers/personGet.swift
+++ Sources/staffDirectory/handlers/personGet.swift
@@ -15,25 +15,40 @@ extension Handlers {
         return {
         request, response in
 
+			// Initial stub object.
+			// Holds either the individual response, or the list response
 			let people = Person()
 
+			// Look for {id} url var in request
 			if let id = request.urlVariables["id"] {
-				people.id = Int(id) ?? 0
-			}
-			if people.id > 0 {
 				do {
-					try people.get()
-					let _ = try? response.setBody(json: people.asDataDict())
+					// Set id via "get" method (aka "Find")
+					try people.get(Int(id) ?? 0)
+
+					// If id is populated, then we have a successful fetch
+					if people.id > 0 {
+						let _ = try? response.setBody(json: people.asDataDict())
+					} else {
+						// Otherwise, exit out of the request
+						throw(ErrorCode.invalidRequest)
+					}
 				} catch {
-					print(error)
+					// Return an error specifying ID fetch failed
+					Handlers.error(request, response, error: "Incorrect ID", code: .badRequest)
+					return
 				}
 			} else {
+				// Execute a list fetch
 				do {
+					// Basic findall method
 					try people.findAll()
 				} catch {
-					print(error)
+					// Return an informative error
+					Handlers.error(request, response, error: "\(error)", code: .badRequest)
+					return
 				}
 
+				// Container for our processed results
 				var data = [String:Any]()
 				for obj in people.rows() {
 					data["\(obj.id)"] = obj.asDataDict()
