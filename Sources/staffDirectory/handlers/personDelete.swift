c2a9982bea7ff383c8288510f1226c880906d846
Jonathan Guthrie <jono@perfect.org>
2017-04-14 11:32:34 -0400

    add code comments and expand on test and error responses

--- Sources/staffDirectory/handlers/personDelete.swift
+++ Sources/staffDirectory/handlers/personDelete.swift
@@ -15,22 +15,41 @@ extension Handlers {
         return {
         request, response in
 
-			let people = Person()
+			// Create stub object
+			let person = Person()
 
+			// Fetch the record ID to be updated from the URL
 			if let id = request.urlVariables["id"] {
-				people.id = Int(id) ?? 0
-			}
-			if people.id > 0 {
 				do {
-					try people.delete()
+					// Test ID for delete by doing a get.
+					try person.get(Int(id) ?? 0)
+				} catch {
+					// Return an informative error
+					Handlers.error(request, response, error: "Incorrect ID", code: .badRequest)
+					return
+				}
+			}
+			
+			// If get (above) has succeeded, then the ID will be populated and we can proceed
+			if person.id > 0 {
+				do {
+					// Attempt deletion
+					try person.delete()
+					
+					// Success
 					let _ = try? response.setBody(json: ["error": "none"])
 				} catch {
-					print(error)
+					// Return an informative error
+					Handlers.error(request, response, error: "\(error)", code: .badRequest)
+					return
 				}
 			} else {
-				let _ = try? response.setBody(json: ["error": "error!"])
+				// Return an error specifying ID fetch failed
+				Handlers.error(request, response, error: "Incorrect ID", code: .badRequest)
+				return
 			}
 
+			// Signal final completion of response
             response.completed()
         }
     }
