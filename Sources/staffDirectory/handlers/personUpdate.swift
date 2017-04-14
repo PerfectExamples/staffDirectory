ba2410deccb3d4906af32d055f4b52c53208b30d
Jonathan Guthrie <jono@perfect.org>
2017-04-14 11:28:53 -0400

    Expand on update process, and comment the code

--- Sources/staffDirectory/handlers/personUpdate.swift
+++ Sources/staffDirectory/handlers/personUpdate.swift
@@ -10,32 +10,56 @@
 import PerfectHTTP
 
 extension Handlers {
-    /// A sample comment for API docs
+    /// API endpoint for handling the update of a record
     static func personUpdate(data: [String:Any]) throws -> RequestHandler {
         return {
         request, response in
 
-			let people = Person()
+			// Create person object
+			let person = Person()
 
+			// Fetch the record ID to be updated from the URL
 			if let id = request.urlVariables["id"] {
-				people.id = Int(id) ?? 0
-			}
-			if people.id > 0 {
 				do {
-					try people.get()
-					if let data = request.postBodyString {
-						let obj = try data.jsonDecode() as? [String:Any]
-						people.firstname = obj?["firstname"] as? String ?? ""
-					}
-					try people.save()
-					let _ = try? response.setBody(json: ["error": "none"])
+					// Test ID for update by doing a get.
+					try person.get(Int(id) ?? 0)
 				} catch {
-					print(error)
+					// Return an informative error
+					Handlers.error(request, response, error: "Incorrect ID", code: .badRequest)
+					return
+				}
+			}
+
+			// If get (above) has succeeded, then the ID will be populated and we can proceed
+			if person.id > 0 {
+				do {
+					// attempt to retrieve record
+					if let data = request.postBodyString {
+
+						// Try to decode JSON
+						let obj = try data.jsonDecode() as? [String:Any]
+
+						// Set properties
+						person.fromDict(obj ?? [String:Any]())
+
+					}
+					// Save record
+					try person.save()
+
+					// Success, so echo back new record
+					let _ = try? response.setBody(json: ["result": person.asDataDict()])
+				} catch {
+					// Return an informative error
+					Handlers.error(request, response, error: "\(error)", code: .badRequest)
+					return
 				}
 			} else {
-				let _ = try? response.setBody(json: ["error": "error!"])
+				// Return an error specifying ID fetch failed
+				Handlers.error(request, response, error: "Incorrect ID", code: .badRequest)
+				return
 			}
 
+			// Signal completion to response object
             response.completed()
         }
     }
