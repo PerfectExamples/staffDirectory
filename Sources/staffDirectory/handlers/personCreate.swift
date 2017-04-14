5bc5d0bbde9ece92ca03fabf51c55aaf22394ac7
Jonathan Guthrie <jono@perfect.org>
2017-04-14 11:05:14 -0400

    Code comments, plus expand to include better error handling

--- Sources/staffDirectory/handlers/personCreate.swift
+++ Sources/staffDirectory/handlers/personCreate.swift
@@ -10,23 +10,49 @@
 import PerfectHTTP
 
 extension Handlers {
-    /// A sample comment for API docs
+    /// API endpoint for handling the creation of a record
     static func personCreate(data: [String:Any]) throws -> RequestHandler {
         return {
         request, response in
 
+			// Create person object
 			let person = Person()
 
+			// Test if the body of the request was submitted with the POST request
 			if let data = request.postBodyString {
 				do {
+					// Decode the post body string as JSON
 					let obj = try data.jsonDecode() as? [String:Any]
-					person.firstname = obj?["firstname"] as? String ?? ""
+
+					// Set properties
+					person.firstname	= obj?["firstname"] as? String ?? ""
+					person.lastname		= obj?["lastname"] as? String ?? ""
+					person.email		= obj?["email"] as? String ?? ""
+					person.address		= obj?["address"] as? String ?? ""
+					person.phone		= obj?["phone"] as? String ?? ""
+
+					// Here, you would do your data validation steps.
+					// For example if you required any fields, test here prior to save, and handle errors gracefully
+					if person.firstname.isEmpty {
+						// Return with an error if "firstname" was empty
+						Handlers.error(request, response, error: "Firstname property cannot be empty", code: .badRequest)
+						return
+					}
+
+					// Save new row
 					try person.save()
 				} catch {
-					print(error)
+					// Return an informative error
+					Handlers.error(request, response, error: "\(error)", code: .badRequest)
+					return
 				}
+			} else {
+				// Return error
+				Handlers.error(request, response, error: "No JSON submitted", code: .badRequest)
+				return
 			}
 
+			// Deliver successful response
 			let _ = try? response.setBody(json: ["result": person.asDataDict()])
             response.completed()
         }
